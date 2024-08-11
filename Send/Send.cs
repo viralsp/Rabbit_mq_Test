17994292522a473d4aec884a73b8fa628fe55248
viral patel <79077532+viralsp@users.noreply.github.com>
2024-08-11 19:00:05 +0530

    Add files via upload
    Send data in Queue
    
    Receive data from Queue

---
+++ Send/Send.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+string message = "Hello World Test";
+
+var bodyMessage  = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange:"",routingKey:"Test",basicProperties:null,body:bodyMessage);
+
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
